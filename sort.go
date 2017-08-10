4c2483934f1597994d4e00b33da7d55f83e45c16
libinbin <libinbin@17paipai.cn>
2017-08-10 21:13:49 +0800

    add uint32 search

--- sort.go
+++ sort.go
@@ -8,5 +8,8 @@ func (p Uint32Slice) Len() int           { return len(p) }
 func (p Uint32Slice) Less(i, j int) bool { return p[i] < p[j] }
 func (p Uint32Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p Uint32Slice) Sort()              { sort.Sort(p) }
+func (p Uint32Slice) Search(x uint32) int {
+	return sort.Search(len(p), func(i int) bool { return p[i] >= x })
+}
 
 func Uint32s(a []uint32) { sort.Sort(Uint32Slice(a)) }
